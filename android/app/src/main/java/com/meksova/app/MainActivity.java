5c9779e46993d8f5d0c029885454b97f0d7ab8f5
Fikre Tewelde <ftewelde1@cscc.edu>
2026-09-05 08:42:28 -0400

    Add Capacitor Android platform

---
+++ android/app/src/main/java/com/meksova/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.meksova.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.meksova.app',
+  appName: 'Meksova',
+  webDir: 'build'
+};
+
+export default config;
